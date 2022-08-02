764bfa8ed80ebf32e3360ccec44950500e8fa85f
Pooven <poovenm@gmail.com>
2022-08-02 21:23:58 +0200

    Added projects for production services

---
+++ src/OrderManagerApi/Controllers/CartsController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace OrderManagerApi.Controllers
+{
+  [Route("[controller]")]
+  [ApiController]
+  public class CartsController : ControllerBase
+  {
+  }
+}
---
+++ src/ProductManagerApi/Controllers/ProductsController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ProductManagerApi.Controllers
+{
+  [Route("[controller]")]
+  [ApiController]
+  public class ProductsController : ControllerBase
+  {
+  }
+}
